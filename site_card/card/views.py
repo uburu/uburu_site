83d2e38fccd94c0411e8ff0182c1b24c9a96d5c8
nikitarub <nikita.rub@yandex.ru>
2018-04-06 22:43:32 +0300

    #1 modified main page

--- site_card/card/views.py
+++ site_card/card/views.py
@@ -3,19 +3,43 @@ from django.http import HttpResponse, Http404
 from django.template import loader
 
 
+# Информация о разаработчиах
+# TODO в будущем перенести в json 
 DATA = [
 dict(id=1, full_name="Никита Рубинов", 
-	short_description_bold="Лидер команды ụbụrụ.", 
-	short_description="Студент МГТУ им. Н.Э. Баумана. Кафедра ИУ-7. Увлекаюсь Deep Learning и web.",
-	projects=["Cайт для подготовки к ЕГЭ и ГИА", "Детектор пауз хезитации", "Умный поиск"]),
+	short_description="Лидер команды ụbụrụ.", 
+	full_description="Студент МГТУ им. Н.Э. Баумана. Кафедра ИУ-7. Студент образовательной программы Технопарк Mail.Ru Group. Увлекаюсь Deep Learning и web.",
+	projects=["Cайт для подготовки к ЕГЭ и ГИА", "Детектор пауз хезитации", "Умный поиск", "Проверка смысловой близости двух текстов"],
+	photo_name= "nikitarub.jpeg"
+	),
+
 dict(
-	id=2, full_name="Иван Морозов",
-	short_description_bold="Программист. Руководитель проектов по компьютерному зрению. ",
-	short_description="Студент МГТУ им. Н.Э. Баумана. Кафедра ИУ-7. Увлекаюсь Компьютерным зрением и web.",
-	projects=["Парсер Instagram без API", "Распознавание минималистичных фотографий", "Умный поиск"]),
+	id= 2,
+	full_name= 'Максим Ермаков',
+	short_description= 'Backend разработчик',
+	full_description= 'Студент МГТУ им Н. Э. Баумана. Кафедра ИУ-7. Образовательная программа Технопарк Mail.Ru Group',
+	projects= ['Telegram бот - доставщик еды', 'Telegram бот - связка ключей, с возможностью генерации новых безопасных паролей', 
+							'Умный поиск', 'Проверка смысловой близости двух текстов',],
+	photo_name= "max.jpeg"
+),
+dict(
+	id=3, full_name="Иван Морозов",
+	short_description="Программист. Разработчик в направлении компьютерное зрение. ",
+	full_description="Студент МГТУ им. Н.Э. Баумана. Кафедра ИУ-7. Увлекаюсь Компьютерным зрением и web.",
+	projects=["Парсер Instagram без API", "Распознавание минималистичных фотографий", "Умный поиск"],
+	photo_name= "ivan.jpeg"
+),
+dict(
+	id=4, full_name="Никита Колобаев", 
+	short_description="Разработчик. Тестировщик.", 
+	full_description="Студент МГТУ им. Н.Э. Баумана. Кафедра ИУ-7. Увлекаюсь аналитикой, проектированием и web",
+	projects=["Умный поиск"],
+	photo_name= "nikitakol.jpeg"
+)
 ]
 
-# Create your views here.
+
+# редерениг основной страницы
 def index(request):
 	data = DATA
 	template = loader.get_template('card/index.html')
